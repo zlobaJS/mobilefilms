1ab636746e83691a8b58f37f6d5d5ce26869b738
Владимир Арутюнов <vladimir.arutyunov@t2.ru>
2025-01-11 01:29:08 +0400

    eeefffff

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,12 @@ const darkTheme = createTheme({
 interface Movie {
   id: number;
   title: string;
+  poster_path: string;
   backdrop_path: string;
+  overview: string;
+  vote_average: number;
+  release_date: string;
+  release_quality?: string; // Если это свойство не обязательно
   // Добавьте другие свойства, если необходимо
 }
 
